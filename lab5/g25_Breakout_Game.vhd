499c8cb9a2c80e345716cdce7832380b41f5fa92
Callum May <callumdmay@gmail.com>
2016-11-22 18:52:38 -0500

    Implement game engine

--- lab5/g25_Breakout_Game.vhd
+++ lab5/g25_Breakout_Game.vhd
@@ -22,15 +22,6 @@ end g25_Breakout_Game;
 
 architecture arch of g25_Breakout_Game is
 
-component g25_video_overlay is
-	port(clock, reset: in std_logic;
-			level, life: in std_logic_vector (2 downto 0);
-			score: in std_logic_vector (15 downto 0);
-			COLUMN, ROW : in unsigned(9 downto 0);
-			RGB : out std_logic_vector(11 downto 0)
-			);
-end component;
-
 component g25_VGA is 
 		port ( clock : in std_logic; -- 50MHz
 		rst : in std_logic; -- reset
@@ -51,96 +42,62 @@ component g25_Renderer is
 	);
 end component;
 
+component g25_Engine is 
+	port (clock, reset : in std_logic;
+			ball_out_row: out unsigned(9 downto 0);
+			ball_out_col: out unsigned(9 downto 0);
+			blocks_out : out std_logic_vector(59 downto 0)		
+	);
+end component;
+
 	signal ROW, COLUMN : unsigned(9 downto 0);
 	signal BLANKING : std_logic;
-	signal int_col, int_row: integer;
-
-	signal score: std_logic_vector(15 downto 0) := "1111111111111111";
-	signal RGB_out : std_logic_vector (11 downto 0);
 	
+	signal RGB_out : std_logic_vector (11 downto 0);
 	signal ball_row: unsigned(9 downto 0) := "0111000010";
 	signal ball_col: unsigned(9 downto 0) := "0110010000";
-	signal col_increment, row_increment: std_logic := '0';
-	signal vo_update_count: std_logic_vector(24 downto 0);
-	signal vo_update : std_logic;
-	signal update_max_value : integer := 1000000;
+	signal score: std_logic_vector(15 downto 0) := "0000000000000000";
 	signal level: std_logic_vector(2 downto 0) := "001";
-	signal life: std_logic_vector(2 downto 0) := "111";
+	signal life: std_logic_vector(2 downto 0) := "101";
+	signal blocks : std_logic_vector (59 downto 0);
 	
 Begin
 
 	R <= RGB_out(11 downto 8);
 	G <= RGB_out(7 downto 4);
 	B <= RGB_out(3 downto 0);
-	
-	int_col <= to_integer(COLUMN)/16;
-	int_row <= to_integer(ROW)/16;
   
 	VGA0 : g25_VGA port map(clock => clock, rst => reset, BLANKING => BLANKING, ROW => ROW, COLUMN => COLUMN, HSYNC => HSYNC, VSYNC => VSYNC);
 	
 	Renderer : g25_Renderer port map(
-			clock => clock,
-			reset => reset,
-			life => life,
-			level => level,
-			score => score,
-			COLUMN => COLUMN,
-			ROW => ROW,
-			ball_col => ball_col,
-			ball_row => ball_row,
-			RGB => RGB_out
-			);
-		
-	--***********Game update clock *********--
-	update_counter : lpm_counter 
-	generic map(LPM_WIDTH => 25)
-	port map (clock =>clock, sclr => vo_update, aclr => reset, q =>vo_update_count);						
-	vo_update <= '1' when vo_update_count = std_logic_vector(to_unsigned(update_max_value, 25)) else
-					 '0';	
-	--*************************************--
-
-	--********** Ball movement **********--
-	update_ball : process(vo_update, reset)
-		variable ball_x: unsigned(9 downto 0) := ball_col;
-		variable ball_y: unsigned(9 downto 0) := ball_row;
+		clock => clock,
+		reset => reset,
+		life => life,
+		level => level,
+		score => score,
+		COLUMN => COLUMN,
+		ROW => ROW,
+		ball_col => ball_col,
+		ball_row => ball_row,
+		RGB => RGB_out
+	);
+			
+	Engine: g25_Engine port map(
+		clock => clock,
+		reset => reset,
+		ball_out_row => ball_row,
+		ball_out_col => ball_col,
+		blocks_out => blocks
+	);
+	
+	reset : process(reset)
 	Begin
-		if (reset = '1') then
-			ball_col <= "0110010000";
-			ball_row <= "0111000010";
-		elsif(rising_edge(vo_update)) then
-			if(col_increment ='1') then
-				ball_x := ball_x + 1;
-			else
-				ball_x := ball_x - 1;
-			end if;
-			
-			if(row_increment ='1') then
-				ball_y := ball_y + 1;
-			else
-				ball_y := ball_y - 1;
-			end if;
-			
-			if(to_integer(ball_x) <= 16) then
-				col_increment <= '1';
-			end if;
-			
-			if(to_integer(ball_x) > 775) then
-				col_increment <= '0';
-			end if;
-			
-			if(to_integer(ball_y) <= 16) then 
-				row_increment <= '1';
-			end if;
-			
-			if(to_integer(ball_y) >= 512)then
-				row_increment <= '0';
-    		end if;
-		
-		end if;
-		
-		ball_col <= ball_x;
-		ball_row <= ball_y;
-			
-	end process update_ball;
+	if(reset ='1') then
+		signal score <= "0000000000000000";
+		signal level <= "001";
+		signal life  <= "101";
+	end if;
+	end process;
+	
 
 end arch;
